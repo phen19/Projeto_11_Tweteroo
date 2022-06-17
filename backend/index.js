c974f7cab10c9415f6c30fc7f5bcbfb96820eded
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-06-17 12:18:33 -0300

    Post tweet implemented

--- backend/index.js
+++ backend/index.js
@@ -3,34 +3,36 @@ import cors from "cors";
 
 
 const server = express();
+server.use(express.json())
 server.use(cors())
 
 const users = []
-
-const tweet = {
-	username: "bobesponja",
-  tweet: "eu amo o hub",
-}
-
-const tweets =[
-	{
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	  tweet: "eu amo o hub"
-	}
-]
+const tweet = []
 
 
 server.post("/sign-up", (req, res) =>{
-    users.push(req)
-    console.log(users)
+    let user = req.body;
+    console.log(user)
+    users.push(user)
     res.send("OK");
   
   } )
 
+  
 
   server.get("/tweets", (req, res) =>{
-    res.send(tweets);
+    
+    
+    res.send(tweet.slice(Math.max(tweet.length - 10, 0)).reverse());
+  
+  } )
+
+  server.post("/tweets", (req, res) =>{
+    let teste = req.body
+    const user = users.find(u=>u.username === teste.username)
+    teste.avatar = user.avatar
+    tweet.push(teste)
+    res.send("OK");
   
   } )
 
